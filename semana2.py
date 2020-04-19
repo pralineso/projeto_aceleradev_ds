b997de1fa2aa8ed04b39d8af74bb9491dc6b8d21
Mariana Xavier <mxpixel@yahoo.com.br>
2020-04-19 18:07:36 -0300

    Comandos interativos com streamlit

--- semana2.py
+++ semana2.py
@@ -2,14 +2,43 @@ import streamlit as st
 
 def main():
     st.title('Hello World')
-    st.header('Esse é o header')
-    st.subheader('Esse é o subheader')
-    st.text('Isto é um texto')
-    st.image('triquetra.png')
-    st.subheader('Audio')
-    #st.audio('record.wav')
-    st.subheader('Video')
-    #st.video('nomevideo.mov')
+    #comandos interativos
+    st.markdown('Botao')
+    botao = st.button('Botao')
+    if botao:
+        st.markdown('Clicado')
+
+    st.markdown('Checkbox')
+    check = st.checkbox('Checkbox')
+    if check:
+        st.markdown('Clicado')
+
+    st.markdown('Radio')
+    radio = st.radio('Escolha as opcoes', ('Opt 1', 'Opt 2'))
+    if radio == 'Opt 1':
+        st.markdown('Opt 1')
+    if radio == 'Opt 2':
+        st.markdown('Opt 2')
+
+    st.markdown('SelectBox')
+    select = st.selectbox('Escolha uma opção', ('...','Opt 1', 'Opt 2'))
+    if select == 'Opt 1':
+        st.markdown('Opt 1')
+    if select == 'Opt 2':
+        st.markdown('Opt 2')
+
+    st.markdown('MultiSelect')
+    multi = st.multiselect('Escolha :', ('Opt 1', 'Opt 2'))
+    if multi == 'Opt 1':
+        st.markdown('Opt 1')
+    if multi == 'Opt 2':
+        st.markdown('Opt 2')
+
+    st.markdown('File uploader')
+    file = st.file_uploader('Choose your file', type = 'csv')
+    if file is not None:
+        st.markdown('Não está vazio!')
+    
 
 if __name__ == '__main__':
     main()
