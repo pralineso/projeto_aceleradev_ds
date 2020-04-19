113dea46b07683f056a5b98c690d9f40e6a140f5
Mariana Xavier <mxpixel@yahoo.com.br>
2020-04-19 19:32:53 -0300

    Iniciando a aplicacao

--- semana2.py
+++ semana2.py
@@ -1,43 +1,18 @@
 import streamlit as st
+import pandas as pd
 
 def main():
-    st.title('Hello World')
-    #comandos interativos
-    st.markdown('Botao')
-    botao = st.button('Botao')
-    if botao:
-        st.markdown('Clicado')
-
-    st.markdown('Checkbox')
-    check = st.checkbox('Checkbox')
-    if check:
-        st.markdown('Clicado')
-
-    st.markdown('Radio')
-    radio = st.radio('Escolha as opcoes', ('Opt 1', 'Opt 2'))
-    if radio == 'Opt 1':
-        st.markdown('Opt 1')
-    if radio == 'Opt 2':
-        st.markdown('Opt 2')
-
-    st.markdown('SelectBox')
-    select = st.selectbox('Escolha uma opção', ('...','Opt 1', 'Opt 2'))
-    if select == 'Opt 1':
-        st.markdown('Opt 1')
-    if select == 'Opt 2':
-        st.markdown('Opt 2')
-
-    st.markdown('MultiSelect')
-    multi = st.multiselect('Escolha :', ('Opt 1', 'Opt 2'))
-    if multi == 'Opt 1':
-        st.markdown('Opt 1')
-    if multi == 'Opt 2':
-        st.markdown('Opt 2')
-
-    st.markdown('File uploader')
+    st.title('Aceleradev Data Science')
+   #st.image('')
     file = st.file_uploader('Choose your file', type = 'csv')
     if file is not None:
-        st.markdown('Não está vazio!')
+       slider = st.slider('Valores', 1,100)
+       df = pd.read_csv(file)
+       st.dataframe(df.head(slider))
+       st.markdown('Markdown')
+       st.table(df.head(slider))
+       st.write(df.columns)
+       st.table(df.groupby('species')['petal_width'].mean())
     
 
 if __name__ == '__main__':
