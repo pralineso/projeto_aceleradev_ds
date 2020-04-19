ab71272cc4c6cac1638edcd38d10a344eb7ec426
Mariana Xavier <mxpixel@yahoo.com.br>
2020-04-19 17:14:51 -0300

    Alguns exemplos de comandos com streamlit

--- semana2.py
+++ semana2.py
@@ -2,6 +2,14 @@ import streamlit as st
 
 def main():
     st.title('Hello World')
+    st.header('Esse é o header')
+    st.subheader('Esse é o subheader')
+    st.text('Isto é um texto')
+    st.image('triquetra.png')
+    st.subheader('Audio')
+    #st.audio('record.wav')
+    st.subheader('Video')
+    #st.video('nomevideo.mov')
 
 if __name__ == '__main__':
     main()
